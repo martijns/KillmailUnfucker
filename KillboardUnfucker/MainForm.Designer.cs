1c169ef77de4e0f8f57927f2e6fdb3529d23bfd0
Martijn Stolk <martijn-git@netripper.nl>
2012-01-09 20:44:07 +0100

    Initial commit, version 0.1  - check clipboard every second, upon finding malformed mail, fix and save in clipboard  - minimize to tray  - beep on fix (if checked)

---
+++ KillboardUnfucker/MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace KillmailUnfucker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.clipboardTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblMailsUnfucked = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chkBeep = new System.Windows.Forms.CheckBox();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trayMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "# of killmails unfucked:";
+            // 
+            // clipboardTimer
+            // 
+            this.clipboardTimer.Interval = 1000;
+            // 
+            // lblMailsUnfucked
+            // 
+            this.lblMailsUnfucked.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMailsUnfucked.Location = new System.Drawing.Point(12, 41);
+            this.lblMailsUnfucked.Name = "lblMailsUnfucked";
+            this.lblMailsUnfucked.Size = new System.Drawing.Size(216, 47);
+            this.lblMailsUnfucked.TabIndex = 1;
+            this.lblMailsUnfucked.Text = "label2";
+            this.lblMailsUnfucked.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(126, 157);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(102, 27);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.HandleExitButtonClick);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Location = new System.Drawing.Point(12, 111);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(216, 40);
+            this.btnMinimize.TabIndex = 3;
+            this.btnMinimize.Text = "Minimize to tray";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.HandleMinimizeToTrayButtonClick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.trayMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.HandleTrayClick);
+            // 
+            // trayMenu
+            // 
+            this.trayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.trayMenu.Name = "trayMenu";
+            this.trayMenu.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.HandleTrayMenuShowClicked);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.HandleTrayMenuExitClicked);
+            // 
+            // chkBeep
+            // 
+            this.chkBeep.AutoSize = true;
+            this.chkBeep.Location = new System.Drawing.Point(12, 88);
+            this.chkBeep.Name = "chkBeep";
+            this.chkBeep.Size = new System.Drawing.Size(102, 17);
+            this.chkBeep.TabIndex = 5;
+            this.chkBeep.Text = "Beep on unfuck";
+            this.chkBeep.UseVisualStyleBackColor = true;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(12, 157);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(102, 27);
+            this.btnAbout.TabIndex = 6;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.HandleAboutClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "by Martijn Stolk";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 203);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.chkBeep);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMailsUnfucked);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Killmail Unfucker";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HandleFormClosed);
+            this.Resize += new System.EventHandler(this.HandleResize);
+            this.trayMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer clipboardTimer;
+        private System.Windows.Forms.Label lblMailsUnfucked;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip trayMenu;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.CheckBox chkBeep;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KillboardUnfucker/MainForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace KillmailUnfucker
+{
+    public partial class MainForm : Form
+    {
+        private int _killmailsUnfucked = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            lblMailsUnfucked.Text = _killmailsUnfucked + "";
+
+            clipboardTimer.Tick += HandleTimerTick;
+            clipboardTimer.Interval = 1000;
+            clipboardTimer.Enabled = true;
+        }
+
+        void HandleTimerTick(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                string txt = Clipboard.GetText();
+                if (IsAndFixedKillmail(ref txt))
+                {
+                    Clipboard.SetText(txt);
+
+                    // Update UI
+                    _killmailsUnfucked++;
+                    lblMailsUnfucked.Text = _killmailsUnfucked + "";
+
+                    // Beep optionally
+                    if (chkBeep.Checked)
+                        SystemSounds.Beep.Play();
+                }
+            }
+        }
+
+        private static string[] killmailCharacteristics = new string[] {
+            "Victim:",
+            "Corp:",
+            "Alliance:",
+            "Faction:",
+            "Destroyed:",
+            "System:",
+            "Security:",
+            "Damage Taken:"
+        };
+
+        protected bool IsAndFixedKillmail(ref string txt)
+        {
+
+            // High speec check for killmail characteristics
+            foreach (string characteristic in killmailCharacteristics)
+            {
+                if (!txt.Contains(characteristic))
+                    return false;
+            }
+
+            // Go through each line
+            string[] lines = txt.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // Find the Security: lines
+                if (lines[i].Contains("Security:"))
+                {
+                    // This is already unfucked if it doesnt have a comma
+                    if (!lines[i].Contains(","))
+                        return false;
+
+                    // Fix this line
+                    lines[i] = lines[i].Replace(',', '.');
+                }
+            }
+
+            // Reassemble as one line
+            txt = string.Join("\n", lines);
+            return true;
+        }
+
+        private void HandleFormClosed(object sender, FormClosedEventArgs e)
+        {
+            clipboardTimer.Enabled = false;
+            notifyIcon.Visible = false;
+        }
+
+        private void HandleExitButtonClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void HandleMinimizeToTrayButtonClick(object sender, EventArgs e)
+        {
+            MinimizeToTray();
+        }
+
+        private void HandleResize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                MinimizeToTray();
+            }
+        }
+
+        private void MinimizeToTray()
+        {
+            notifyIcon.Visible = true;
+            Hide();
+        }
+
+        private void RestoreFromTray()
+        {
+            notifyIcon.Visible = false;
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void HandleTrayClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                RestoreFromTray();
+            }
+        }
+
+        private void HandleTrayMenuShowClicked(object sender, EventArgs e)
+        {
+            RestoreFromTray();
+        }
+
+        private void HandleTrayMenuExitClicked(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void HandleAboutClick(object sender, EventArgs e)
+        {
+            MessageBox.Show(this,
+                "Since CCP added localization to generated killmails in the Eve Online client, some " +
+                "killboards will complain about malformed killmails. This utility monitores the clipboard and will " +
+                "fix the killmail if any localized features are found.\r\n" +
+                "\r\n" +
+                "Currently it only replaced the comma with a dot in lines that indicate security.\r\n" +
+                "\r\n" +
+                "~ Martijn Stolk ~", "Killmail Unfucker");
+        }
+
+    }
+}
